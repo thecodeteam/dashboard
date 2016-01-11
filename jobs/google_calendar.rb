a9e19e4f6df397f5195e44cb8344e8308a0d9bcc
jonasrosland <jonas.rosland@gmail.com>
2016-01-11 15:08:16 -0500

    Updated Google calendar URL

--- jobs/google_calendar.rb
+++ jobs/google_calendar.rb
@@ -1,10 +1,17 @@
 require 'icalendar'
 
-ical_url = 'https://www.google.com/calendar/ical/52rlkjj3h1lsfqmi5hr0475ceg%40group.calendar.google.com/public/basic.ics'
+ical_url = 'https://calendar.google.com/calendar/ical/52rlkjj3h1lsfqmi5hr0475ceg%40group.calendar.google.com/private-a59d0bc7442a6bf35df6d8ea8a13ad03/basic.ics'
+#ical_url = 'https://www.google.com/calendar/ical/52rlkjj3h1lsfqmi5hr0475ceg%40group.calendar.google.com/public/basic.ics'
 uri = URI ical_url
 
 SCHEDULER.every '15s', :first_in => 4 do |job|
-  result = Net::HTTP.get uri
+#  result = Net::HTTP.get uri
+  parsed_url = URI.parse(ical_url)
+  puts parsed_url
+  http = Net::HTTP.new(parsed_url.host, parsed_url.port)
+  http.use_ssl = (parsed_url.scheme == "https")
+  req = Net::HTTP::Get.new(parsed_url.request_uri)
+  result = http.request(req).body
   calendars = Icalendar.parse(result)
   calendar = calendars.first
 
